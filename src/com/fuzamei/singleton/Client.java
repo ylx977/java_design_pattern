669d9ac1764df3ebb1ff40aa67af54955a3dd7fc
杨凌霄 <ylx@disanbo.com>
2018-08-20 17:51:06 +0800

    first commit

---
+++ src/com/fuzamei/singleton/Client.java
@@ -0,0 +1,15 @@
+package com.fuzamei.singleton;
+
+/**
+ * @author ylx
+ * Created by fuzamei on 2018/8/20.
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/com/fuzamei/singleton/LazyModeSingleton.java
@@ -0,0 +1,11 @@
+package com.fuzamei.singleton;
+
+/**
+ * @author ylx
+ * Created by fuzamei on 2018/8/20.
+ */
+public class LazyModeSingleton {
+
+
+
+}
